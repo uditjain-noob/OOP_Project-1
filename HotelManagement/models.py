a62d319a20bb0f0a3cfccb3c603179800674c97b
Rohit-Annamaneni-1207 <82631318+Rohit-Annamaneni-1207@users.noreply.github.com>
2021-11-22 10:11:52 +0530

    Replacing initial files
    Replacing initial files

---
+++ HotelManagement/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models.fields import CharField, IntegerField, DateField
+from django.utils import timezone
+# Create your models here.
+
+class Room(models.Model):
+    room_num = models.IntegerField(default=0, primary_key=True)
+    room_type = models.CharField(max_length=50, default=" ")
+
+class Schedule(models.Model):
+    # id = models.IntegerField(default=0, primary_key=True)
+    room_num = models.IntegerField(default=0)
+    check_in = models.DateField(default = timezone.now)
+    check_out = models.DateField(default = timezone.now)
+
+class User(models.Model):
+    # id = models.IntegerField(default=0, primary_key=True, auto_created=True)
+    name = models.CharField(max_length=50, default=" ")
+    email = models.CharField(max_length=100, default=" ")
+    phone_num = models.IntegerField(default=0)
+    encrypt_pwd = models.CharField(max_length=50, default=" ")
+    rooms = models.CharField(max_length=150, default = " ")
---
+++ HotelManagement/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home-page"),
+    path('register/', views.register, name="registration-page")
+]
