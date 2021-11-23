bca5a2d0f36714dfca38a8bd47366c71f573799f
Rohit-Annamaneni-1207 <82631318+Rohit-Annamaneni-1207@users.noreply.github.com>
2021-11-23 10:37:18 +0530

    commit 2

--- HotelManagement/views.py
+++ HotelManagement/views.py
@@ -7,4 +7,11 @@ def home(request):
     return render(request, 'HotelManagement/index.html')
 
 def register(request):
+    if request.method == "POST":
+        Name = request.POST.get('Name', '')
+        Email = request.POST.get('Email', '')
+        phone_num = request.POST.get('PhoneNo','')
+        PWD = request.POST.get('PWD', '')
+        
+        Reg = User(name = Name, )
     return render(request, 'HotelManagement/register.html')
