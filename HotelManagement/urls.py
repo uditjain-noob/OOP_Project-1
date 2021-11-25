1dddead0bf26edd5cce765210c73f57648faddf8
Rohit-Annamaneni-1207 <82631318+Rohit-Annamaneni-1207@users.noreply.github.com>
2021-11-25 20:55:02 +0530

    email validation

--- HotelManagement/urls.py
+++ HotelManagement/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home-page"),
-    path('register/', views.register, name="registration-page")
+    path('register/', views.register, name="registration-page"),
+    path('verify/', views.verify, name="verification-page")
 ]
