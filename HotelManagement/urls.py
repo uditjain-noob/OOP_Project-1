d2852adae00b8bcbe9b38a297ba58f3f5f348e1d
Rohit-Annamaneni-1207 <82631318+Rohit-Annamaneni-1207@users.noreply.github.com>
2021-11-26 19:20:12 +0530

    index.html design

--- HotelManagement/urls.py
+++ HotelManagement/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home-page"),
+    path('login/', views.login, name="login-page"),
+    path('verify/', views.verify, name="verification-page"),
     path('register/', views.register, name="registration-page")
 ]
--- HotelManagement/views.py
+++ HotelManagement/views.py
@@ -4,21 +4,47 @@ from django.core.mail import send_mail
 import requests
 from string import ascii_lowercase, ascii_uppercase, digits
 from random import choices
-# Create your views here.
 
+import HotelManagement
+# Create your views here.
+# global arr
+# arr = {
+#     "Name" : None,
+#     "Email" : None,
+#     "phone_num" : None,
+#     "PWD": None,
+#     "rooms": None,
+#     "checkin": None,
+#     "checkout":None,
+#     "verification_code": None
+# }
 
 def home(request):
     return render(request, 'HotelManagement/index.html')
 
-def register(request):
+def login(request):
+    return render(request, 'HotelManagement/login.html')
+
+def register(request):  
+    return render(request, 'HotelManagement/register.html')
+
+def verify(request):
+    context = {
+                "Name" : None,
+                "Email" : None,
+                "PWD": None,
+                "verification_code": None
+              }
+
     if request.method == "POST":
         Name = request.POST.get('Name', '')
         Email = request.POST.get('Email', '')
-        phone_num = request.POST.get('PhoneNo','')
+        # phone_num = request.POST.get('PhoneNo','')
         PWD = request.POST.get('PWD', '')
         print(Email)
 
         random_str = "".join(choices(ascii_uppercase + ascii_lowercase + digits, k=6))
+        # random_str = request.POST.get('Verification Code', '')
 
         url = "https://email-sender1.p.rapidapi.com/"
 
@@ -36,9 +62,11 @@ def register(request):
                      }"""
 
         response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-
         print(response.text)
 
-        # print(type(Email))
-        # Reg = User(name = Name, )
-    return render(request, 'HotelManagement/register.html')
+        context["Name"] = Name
+        context["Email"] = Email
+        context["PWD"] = PWD
+        context["verification_code"] = random_str
+
+    return render(request, 'HotelManagement/verify.html', context)
