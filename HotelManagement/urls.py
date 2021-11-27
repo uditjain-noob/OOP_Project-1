207ff4abea21e30f2030f03ef2cf67a8f48ab66a
AmitTalawar <talawaramit80@gmail.com>
2021-11-27 12:11:14 +0530

    CSS design changes

--- HotelManagement/urls.py
+++ HotelManagement/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home-page"),
+    path('login/', views.login, name="login-page"),
+    path('verify/', views.verify, name="verification-page"),
     path('register/', views.register, name="registration-page")
+    # path('register/', views.register, name="registration-page")
 ]
